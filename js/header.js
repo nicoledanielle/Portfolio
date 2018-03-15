8359a96fed02d0d6790f3558feb5c19ead610815
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-03-15 11:25:57 -0700

    Header section -- added mobile friendly navigation including JS

--- js/header.js
+++ js/header.js
@@ -1,34 +1,9 @@
 /* global $ */
 'use strict';
 
-function createString(input) {
-  $(input).each(function() {
-    let myStr = $(this).html();
-    myStr = myStr.split('');
-     
-    let contents = '';
-    for (let i = 0, len = myStr.length; i < len; i++) {
-      contents += '<span class="single-char char-' +  i +  '">' + myStr[i] +  '</span>';
-    }
-    $(input).html(contents);
+$(document).ready(function(){
+  $('.mobile-arrow').click(function() {
+    console.log('here');
+    $('.main-nav-item').toggleClass('hide');
   });
-}
-
-$('a').each(function() {
-  var a = new RegExp('/' + window.location.host + '/');
-  if(!a.test(this.href)) {
-    $(this).click(function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      window.open(this.href, '_blank');
-    });
-  }
 });
- 
-$('document').ready(function() {
-  let myHeader = $('#main-header');
-  createString(myHeader);
-  $('.char-3').addClass('sky-blue');
-  $('.char-10').addClass('sky-blue');
-  $('.char-5').addClass('spacing');
-}); 
